6160bb13bccf154f0c2b2f93334d842ed243743b
alvinKyle <alvinkyle70@gmail.com>
2024-06-02 18:46:20 +0300

    updated

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 15, "M", "brown", "cheeky", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 10, "M", "black", "shy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 17, "F", "white", "stubborn", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 20, "F", "red", "active", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 5, "F", "white", "nonchalant", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 9, "M", "ginger", "playful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 17, "F", "black", "happy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 5, "M", "brown", "cheeky", 0);
